717fe4f683288077fe5e50f9a14d7e08e1d33518
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-12-04 00:37:07 +0530

    Created Product, Category and Images entity and mapped them using JPA annotation

--- src/main/java/com/insomniacScribbler/chauhan_stores/model/Image.java
+++ src/main/java/com/insomniacScribbler/chauhan_stores/model/Image.java
@@ -1,6 +1,7 @@
 package com.insomniacScribbler.chauhan_stores.model;
 
 import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 import java.sql.Blob;
@@ -13,5 +14,10 @@ public class Image {
     private String downloadUrl;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    /*
+    * The @JoinColumn annotation specifies the foreign key column name in the database.
+    * In this case, it creates a column called product_id in the current table that references the Product table's primary key.
+    * */
     private Product product;
 }
